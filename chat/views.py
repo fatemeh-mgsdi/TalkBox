cf3a02e9d3e0d1a571437a0fe2bc48141aa66da0
fatemeh-mgsdi <fatemeh.maghsoudi76@gmail.com>
2023-06-09 22:54:52 +0330

    Add endpoint for retrieving room by id

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import ChatRoom, ChatMessage
 from .serializers import ChatRoomSerializer, ChatMessageSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -8,6 +9,12 @@ class ChatRoomViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = ChatRoomSerializer
     queryset = ChatRoom.objects.all()
+
+    def retrieve(self, request, pk=None):
+        chatroom = ChatRoom.objects.get(id=pk)
+        serializer = ChatRoomSerializer(chatroom)
+        return Response(serializer.data)
+
 
 
 class ChatMessageViewSet(viewsets.ModelViewSet):
